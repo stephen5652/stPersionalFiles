e890bc410693c7c8834106c19bc855385820f5d0
stephen5652 <stephen5652@126.com>
2023-06-09 11:30:41 +0800

    chore:  deleted files

--- nvim/lua/initial.lua
+++
@@ -1,5 +0,0 @@
-vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
-
-require("plugins.lazy-nvim")
-
-require("core.options")
--- nvim/lua/plugins/toggleterm.lua
+++
@@ -1,7 +0,0 @@
-local opt = require("toggleterm").setup({
-	open_mapping = [[<c-\>]],
-	-- 打开新终端后自动进入插入模式
-	start_in_insert = true,
-	-- 在当前buffer的下方打开新终端
-	direction = "horizontal",
-})
